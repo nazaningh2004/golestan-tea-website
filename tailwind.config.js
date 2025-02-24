3d62e7c87d1009ba93db33d9c950a1b6702091f6
nazaningh2004 <nazanin.ghanbari400@gmail.com>
2025-02-24 15:06:40 +0330

    update: new changes on chay-golestan project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./*.html"],
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html"],
   theme: {
     extend: {
-      screens:{
-        mobile: "0",
+      screens: {
+        mobile: "0px",
         tablet: "768px",
-        desktop:"1024px",
+        desktop: "1024px",
       },
       fontFamily: {
-        'iransans': ['Iransans'],
+        iransans: ["Iransans"],
       },
     },
   },
   plugins: [],
-}
-
+};
